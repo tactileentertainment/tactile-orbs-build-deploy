e5a070c435d6b4661a4c6ee3639971ec92d63bce
Rasmus Ljungmann Pedersen <rasmuslp@gmail.com>
2021-10-21 10:01:40 +0200

    feat: Add commands for gcloud-access and dryrun deploy, and job for dryrun

---
+++ src/scripts/dryrun-to-k8s-cluster.sh
@@ -0,0 +1,10 @@
+dryrunToK8sCluster() {
+  gcloud container clusters get-credentials --zone "$CLUSTER_ZONE" "$CLUSTER_NAME"
+  kubectl apply --cluster gke_tactile-webservices_"$CLUSTER_ZONE"_"$CLUSTER_NAME" --validate --dry-run=server -f - < "$CLUSTER_NAME".yml
+}
+
+# Will not run if sourced for bats-core tests.
+ORB_TEST_ENV="bats-core"
+if [ "${0#*$ORB_TEST_ENV}" == "$0" ]; then
+    dryrunToK8sCluster
+fi
---
+++ src/scripts/gcloud-access.sh
@@ -0,0 +1,10 @@
+gcloudAccess() {
+  echo "$GCLOUD_SERVICE_KEY" | gcloud auth activate-service-account --key-file=-
+  gcloud --quiet config set project "$GOOGLE_PROJECT_ID"
+}
+
+# Will not run if sourced for bats-core tests.
+ORB_TEST_ENV="bats-core"
+if [ "${0#*$ORB_TEST_ENV}" == "$0" ]; then
+    gcloudAccess
+fi
