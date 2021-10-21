72de6f326941b35511ea121e6126e46e9b7a269d
Rasmus Ljungmann Pedersen <rasmuslp@gmail.com>
2021-10-21 10:26:19 +0200

    feat(dryrun): Add parameter for specifying the configuration to apply

--- src/scripts/dryrun-to-k8s-cluster.sh
+++ src/scripts/dryrun-to-k8s-cluster.sh
@@ -1,6 +1,6 @@
 dryrunToK8sCluster() {
   gcloud container clusters get-credentials --zone "$CLUSTER_ZONE" "$CLUSTER_NAME"
-  kubectl apply --cluster gke_tactile-webservices_"$CLUSTER_ZONE"_"$CLUSTER_NAME" --validate --dry-run=server -f - < "$CLUSTER_NAME".yml
+  kubectl apply --cluster gke_tactile-webservices_"$CLUSTER_ZONE"_"$CLUSTER_NAME" --validate --dry-run=server -f - < "$CONFIGURATION_PATH"
 }
 
 # Will not run if sourced for bats-core tests.
