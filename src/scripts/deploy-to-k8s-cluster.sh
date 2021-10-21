0b096d0f99d4744dc417f98ad71205c6d10b6c8f
Rasmus Ljungmann Pedersen <rasmuslp@gmail.com>
2021-10-21 11:14:45 +0200

    feat(deploy): Add job and command for deploying to k8s

--- src/scripts/deploy-to-k8s-cluster.sh
+++ src/scripts/deploy-to-k8s-cluster.sh
@@ -1,10 +1,10 @@
-dryrunToK8sCluster() {
+deployToK8sCluster() {
   gcloud container clusters get-credentials --zone "$CLUSTER_ZONE" "$CLUSTER_NAME"
-  kubectl apply --cluster gke_tactile-webservices_"$CLUSTER_ZONE"_"$CLUSTER_NAME" -f "$CONFIGURATION_PATH" --validate --dry-run=server
+  kubectl apply --cluster gke_tactile-webservices_"$CLUSTER_ZONE"_"$CLUSTER_NAME" -f "$CONFIGURATION_PATH" --validate --prune --selector "$PRUNE_SELECTOR"
 }
 
 # Will not run if sourced for bats-core tests.
 ORB_TEST_ENV="bats-core"
 if [ "${0#*$ORB_TEST_ENV}" == "$0" ]; then
-    dryrunToK8sCluster
+    deployToK8sCluster
 fi
