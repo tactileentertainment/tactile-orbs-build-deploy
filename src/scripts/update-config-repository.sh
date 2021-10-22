de8b3292817389610ddd60c2d2ed29c2a3af5503
Rasmus Ljungmann Pedersen <rasmuslp@gmail.com>
2021-10-22 14:56:23 +0200

    feat: Added command for making changes in the configuration repository

---
+++ src/scripts/update-config-repository.sh
@@ -0,0 +1,20 @@
+updateConfigRepository() {
+  cd ..
+  git clone "$CONFIG_REPOSITORY" config-repository
+  # shellcheck disable=SC2164
+  cd config-repository
+  git checkout master
+
+  bash "$CONFIG_UPDATE_COMMAND"
+
+  git commit -m "build(release): $CIRCLE_PROJECT_REPONAME $IMAGE_TAG" -a
+  git tag -a "$IMAGE_TAG" -m "$CIRCLE_PROJECT_REPONAME $IMAGE_TAG"
+  git push origin master --tags
+  git push origin master:"$CONFIG_BRANCH"
+}
+
+# Will not run if sourced for bats-core tests.
+ORB_TEST_ENV="bats-core"
+if [ "${0#*$ORB_TEST_ENV}" == "$0" ]; then
+    updateConfigRepository
+fi
