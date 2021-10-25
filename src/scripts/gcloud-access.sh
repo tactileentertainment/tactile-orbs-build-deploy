019f37b0e79b8c45dddc825d9845655574d621bc
Rasmus Ljungmann Pedersen <rasmuslp@gmail.com>
2021-10-25 14:43:26 +0200

    refactor(gcloud-access): Standardise ENV VAR naming

--- src/scripts/gcloud-access.sh
+++ src/scripts/gcloud-access.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 gcloudAccess() {
-  echo "$GCLOUD_SERVICE_KEY" | gcloud auth activate-service-account --key-file=-
-  gcloud --quiet config set project "$GOOGLE_PROJECT_ID"
+  echo "$GCP_SERVICE_KEY" | gcloud auth activate-service-account --key-file=-
+  gcloud --quiet config set project "$GCP_PROJECT"
 }
 
 # Will not run if sourced for bats-core tests.
